6f614780d7a92c297ed11bcd9d88bc6f7e37a7c2
Siam-al-islam <siamalislam692@gmail.com>
2024-04-01 19:32:56 +0600

    area calculation part done

--- js/triangle-calculate.js
+++ js/triangle-calculate.js
@@ -1,9 +1,11 @@
 function calculateTriangleArea() {
     const triangleBaseInput = document.getElementById('triangle-base').value;
     const triangleHeightInput = document.getElementById('triangle-height').value;
+    const triangleSpan = document.getElementById('triangle-area');
     const base = parseFloat(triangleBaseInput);
     const height = parseFloat(triangleHeightInput)
     const calculateArea = 0.5 * base * height;
+    triangleSpan.innerText = calculateArea;
     const triangleSection = document.getElementById('triangle-sec');
     const result = document.createElement('p');
     result.innerText = calculateArea;
